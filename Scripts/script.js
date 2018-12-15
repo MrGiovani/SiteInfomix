fb6b2f6b2f556c77bdefb1eba05470c9c2c3f301
MrGiovani <giovanigg08@gmail.com>
2018-12-15 17:48:03 -0200

    Site Infomix - Desktop

---
+++ Scripts/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+$('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+   });
+
+autoplay();
+function autoplay() {
+    $('.carousel').carousel('next');
+    setTimeout(autoplay, 4500);
+}
+
+$(document).ready(function () {
+    $("a.scrollLink").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 1000);
+    });
+});
+
+$(document).ready(function(){
+    $('.tooltipped').tooltip();
+});
+
+$(document).ready(function(){
+    $('#send').click(function() {
+        var mensagem = document.getElementById('mensagem').value;
+        var nome = document.getElementById('nome').value;
+        window.open('mailto:giovanigg08@gmail.com?subject=Contato WebSite de: ' + nome + '&body='+mensagem);
+    });
+});
