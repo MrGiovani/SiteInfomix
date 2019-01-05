2e0a2c675bb2f5672df56f8a9da8b3980867465d
MrGiovani <giovanigg08@gmail.com>
2019-01-04 22:59:01 -0200

    Informações corretas email

--- email.php
+++ email.php
@@ -8,26 +8,26 @@
 
     if (isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['mensagem']) && !empty($_POST['mensagem'])) {
         $email = $_POST['email'];
-        $mensagem = $_POST['mensagem'] . '<br><br>E-mail de quem entrou em contato: ' . $email;
+        $mensagem = $_POST['mensagem'] . '<br><br>E-mail do cliente que entrou em contato: ' . $email . '<br><br>Esta é uma mensagem automática. Por favor, não a responda, direcione a resposta ao email do cliente.'
         $nome = $_POST['nome'];
         try {
             $mail = new PHPMailer;
 
             $mail->SMTPDebug = 2;
             $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
+            $mail->Host = 'mail.figdigital.com.br';
             $mail->SMTPAuth = true;
             $mail->SMTPSecure = 'tls';
-            $mail->Username = 'paulapauzuda@gmail.com';
-            $mail->Password = 'meupaunatuamao';
+            $mail->Username = 'no-reply@figdigital.com.br';
+            $mail->Password = 'semsenha';
             $mail->Port = 587;
 
             $mail->setFrom($email, $nome);
-            $mail->addAddress('paulapauzuda@gmail.com');
+            $mail->addAddress('diogo@infomixonline.com');
 
             $mail->isHTML(true);
 
-            $mail->Subject = 'Contato via website';
+            $mail->Subject = 'CONTATO VIA WEBSITE';
             $mail->Body    = nl2br($mensagem);
             $mail->AltBody = nl2br(strip_tags($mensagem));
 
